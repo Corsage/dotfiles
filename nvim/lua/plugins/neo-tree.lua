b39e28a84c2306040f5aece13a750f3b910110f0
Jay Chowdhary <crsg@rebirth.dev>
2024-05-02 18:19:52 -0700

    Updated neo-tree.

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,23 +1,43 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-    },
-    config = function()
-      -- require("neo-tree").setup({
-      --   mappings = {
-      --     ['n'] = {
-      --       "add",
-      --       config = {
-      --         show_path = "none"
-      --       }
-      --     },
-      --   },
-      -- })
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			filesystem = {
+				hijack_netrw_behavior = "open_current",
+			},
+			buffers = {
+				follow_current_file = {
+					enabled = true, -- This will find and focus the file in the active buffer every time
+					leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+				},
+			},
+			event_handlers = {
+				{
+					event = "file_opened",
+					handler = function(file_path)
+						-- auto close
+						-- vimc.cmd("Neotree close")
+						-- OR
+						require("neo-tree.command").execute({ action = "close" })
+					end,
+				},
+				{
+					event = "neo_tree_window_after_open",
+					handler = function(args)
+						if args.position == "left" or args.position == "right" then
+							vim.cmd("wincmd =")
+						end
+					end,
+				},
+			},
+		})
 
-      vim.keymap.set("n", "<C-e>", ":Neotree filesystem reveal left<CR>", {})
-    end
+		vim.keymap.set("n", "<C-e>", ":Neotree filesystem reveal left<CR>", {})
+	end,
 }
