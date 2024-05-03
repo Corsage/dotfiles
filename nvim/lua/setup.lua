3b38e7c5933384dcad89ec6988590b6fa82be58f
Jay Chowdhary <crsg@rebirth.dev>
2024-05-02 18:26:20 -0700

    Added more options.

--- nvim/lua/setup.lua
+++ nvim/lua/setup.lua
@@ -3,9 +3,17 @@ vim.g.mapleader = " "
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
 
 -- Use nushell as default shell.
-vim.opt.shell = 'nu'
-vim.opt.shellcmdflag = '-c'
+vim.opt.shell = "nu"
+vim.opt.shellcmdflag = "-c"
 vim.opt.shellquote = ""
 vim.opt.shellxquote = ""
+vim.opt.termguicolors = true
+
+vim.opt.smartindent = true
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
